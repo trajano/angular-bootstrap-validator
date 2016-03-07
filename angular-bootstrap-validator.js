0739cdd1e6a4f5d108035573c33fb873e8793ed0
Archimedes Trajano <arch@trajano.net>
2016-03-07 00:23:22 -0500

    Removed the pre

--- angular-bootstrap-validator.js
+++ angular-bootstrap-validator.js
@@ -1,9 +1,7 @@
-var NG_HIDE_CLASS = 'ng-hide';
-var NG_HIDE_IN_PROGRESS_CLASS = 'ng-hide-animate';
-
 angular.module('angular.bootstrap.validator', [])
   .directive('abvIdentical', function() {
     return {
+      restrict: 'A',
       scope: {
         targetModel: '=abvIdentical'
       },
@@ -23,6 +21,7 @@ angular.module('angular.bootstrap.validator', [])
   })
   .directive('abvDifferent', function() {
     return {
+      restrict: 'A',
       scope: {
         targetModel: '=abvDifferent'
       },
@@ -43,63 +42,52 @@ angular.module('angular.bootstrap.validator', [])
   .directive('abvForm', function($compile) {
     return {
       require: 'form',
+      restrict: 'A',
       terminal: true,
-      compile: (element, attrs) => {
-        return {
-          pre: (scope, element, attr, controller) => {
-            var formName = controller.$name;
-            var formNameSubmittedAnd = formName + '.$submitted && ';
+      compile: function (tElement, tAttrs) {
+        return (scope, element, attr, controller) => {
+          var formName = controller.$name;
+          var formNameSubmittedAnd = formName + '.$submitted && ';
 
-            // disable submit buttons when the form is invalid after submit
-            element.find('button[type="submit"]').attr("ng-disabled", formNameSubmittedAnd + formName + '.$invalid');
-            element.attr('novalidate', 'novalidate');
+          // disable submit buttons when the form is invalid after submit
+          element.find('button[type="submit"]').attr("ng-disabled", formNameSubmittedAnd + formName + '.$invalid');
 
-            // for each form-control in a form group
-            element.find('.form-group').each((index, formGroup) => {
-              var controls = $(formGroup).find('.form-control');
-              // Only input text areas should have feedback, textarea must not have feedback
-              // only support one control per form-group as per Bootstrap CSS examples
-              if (controls.length == 1) {
-                // set the name. and ng-model appropriately
-                var control = $(controls[0]);
-                var controlName = control.attr('name');
-                if (!controlName) {
-                  controlName = control.attr('id');
-                  control.attr('name', controlName);
-                }
-                if (!control.attr('ng-model') && !control.attr('data-ng-model')) {
-                  control.attr('ng-model', controlName);
-                }
+          // disable native form validation because the validation is handled by Angular
+          element.attr('novalidate', 'novalidate');
 
-                // add feedback icons
-                if ($(formGroup).hasClass('has-feedback')) {
-                  control.after('<span class="glyphicon glyphicon-ok form-control-feedback" ng-if="' + formNameSubmittedAnd + formName + '.' + controlName + '.$valid' + '" aria-hidden="true"></span>');
-                  control.after('<span class="glyphicon glyphicon-remove form-control-feedback" ng-if="' + formNameSubmittedAnd + formName + '.' + controlName + '.$invalid' + '" aria-hidden="true"></span>');
-                }
-
-                $(formGroup).attr('ng-class', '{"has-error": ' + formNameSubmittedAnd + formName + '.' + controlName + '.$invalid }');
-                // help blocks for error messages
-
-                $(formGroup).find('.help-block[data-abv-error],.help-block[abv-error]').each((index, helpBlock) => {
-                  var errorField = $(helpBlock).attr('data-abv-error');
-                  if (!errorField) {
-                    errorField = $(helpBlock).attr('abv-error');
-                  }
-                  $(helpBlock).attr('ng-show', formNameSubmittedAnd + formName + '.' + controlName + '.$error.' + errorField);
-                });
+          // for each form-control in a form group
+          element.find('.form-group').each((index, formGroup) => {
+            var controls = $(formGroup).find('.form-control');
+            // Only input text areas should have feedback, textarea must not have feedback
+            // only support one control per form-group as per Bootstrap CSS examples
+            if (controls.length == 1) {
+              // set the name. and ng-model appropriately
+              var control = $(controls[0]);
+              var controlName = control.attr('name');
+              if (!controlName) {
+                controlName = control.attr('id');
+                control.attr('name', controlName);
               }
-              //                          element.unbind();
-            });
-            //       element.removeAttr('abv-form');
-            //$compile(element.contents())(scope);
+              if (!control.attr('ng-model') && !control.data('ng-model')) {
+                control.attr('ng-model', controlName);
+              }
 
-            // disable native form validation because the validation is handled by Angular
-          },
-          post: (scope, element, attr, controller) => {
-            var formName = attr.name;
-            var formNameSubmittedAnd = formName + '.$submitted && ';
-            $compile(element.contents())(scope);
-          }
+              // add feedback icons
+              if ($(formGroup).hasClass('has-feedback')) {
+                control.after('<span class="glyphicon glyphicon-ok form-control-feedback" ng-if="' + formNameSubmittedAnd + formName + '.' + controlName + '.$valid' + '" aria-hidden="true"></span>');
+                control.after('<span class="glyphicon glyphicon-remove form-control-feedback" ng-if="' + formNameSubmittedAnd + formName + '.' + controlName + '.$invalid' + '" aria-hidden="true"></span>');
+              }
+
+              $(formGroup).attr('ng-class', '{"has-error": ' + formNameSubmittedAnd + formName + '.' + controlName + '.$invalid }');
+              // help blocks for error messages
+
+              $(formGroup).find('.help-block[data-abv-error],.help-block[abv-error]').each((index, helpBlock) => {
+                var errorField = $(helpBlock).data('abv-error') || $(helpBlock).attr('abv-error');
+                $(helpBlock).attr('ng-show', formNameSubmittedAnd + formName + '.' + controlName + '.$error.' + errorField);
+              });
+            }
+          });
+          $compile(element.contents())(scope);
         };
       }
     };
