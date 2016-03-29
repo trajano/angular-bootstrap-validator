9b0d5749d0e83e3b7c1c4ab173ae043d63e28c1b
Archimedes Trajano <arch@trajano.net>
2016-03-28 23:48:57 -0400

    updated Meteor package version and cleanup

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: 'trajano:angular-bootstrap-validator',
   summary: 'Provides a directive that enables validations using AngularJS with forms that are marked up.',
-  version: '1.0.2',
+  version: '1.1.0_1',
   git: 'https://github.com/trajano/angular-bootstrap-validator',
   documentation: 'README.md'
 })
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.1')
-  api.use(['jquery', 'angular@1.3.7', 'twbs:bootstrap@3.3.6'])
+  api.use(['jquery', 'angular@1.3.7', 'twbs:bootstrap@3.3.6'], ['client'])
   api.addFiles(['src/angular-bootstrap-validator.js',
     'src/directives/abvDifferent.js',
     'src/directives/abvError.js',
